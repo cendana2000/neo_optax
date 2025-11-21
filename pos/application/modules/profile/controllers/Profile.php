1a642aa14a7145c88628815281dab42ff0fcc547
ivanardi07 <ardiyanto.ivan07@gmail.com>
2025-11-21 09:27:40 +0700

    pos-project bugfix module profile v2

--- pos/application/modules/profile/controllers/Profile.php
+++ pos/application/modules/profile/controllers/Profile.php
@@ -49,7 +49,7 @@ class Profile extends BASE_Controller
             $user_pass = $user['user_password'];
             $new_pass = $this->password($data['password_new']);
             $old_pass = $this->password($data['password_old']);
-            if($user_pass == $old_pass){
+            if ($user_pass == $old_pass) {
                 $update = $this->User->update(['user_id' => $user_id], ['user_password' => $new_pass]);
                 if ($update['success']) {
                     $operation = [
@@ -62,7 +62,7 @@ class Profile extends BASE_Controller
                         'message' => 'Failed to change password !'
                     ];
                 }
-            }else{
+            } else {
                 $operation = [
                     'success' => false,
                     'message' => 'Wrong Old Password'
@@ -102,60 +102,77 @@ class Profile extends BASE_Controller
     public function update()
     {
         $data = varPost();
+        $user_id = $data['user_id'];
         if (!file_exists("./dokumen/user")) {
             mkdir("./dokumen/user", 0777, true);
             mkdir("./dokumen/user/thumbs", 0777, true);
         }
-        $filess = $_FILES['user_foto']['name'];
-        $config['upload_path'] = "./dokumen/user";
-        $config['file_name'] = gen_uuid($this->User->get_table());
-        $config['allowed_types'] = 'jpg|JPG|jpeg|JPEG|png|PNG';
-        if ($filess) {
-            $read = $this->User->read($data['user_id']);
-            $this->upload->initialize($config);
-            $_FILES['upload_field_name']['name']        = $_FILES['user_foto']['name'];
-            $_FILES['upload_field_name']['type']        = $_FILES['user_foto']['type'];
-            $_FILES['upload_field_name']['tmp_name']    = $_FILES['user_foto']['tmp_name'];
-            $_FILES['upload_field_name']['error']       = $_FILES['user_foto']['error'];
-            $_FILES['upload_field_name']['size']        = $_FILES['user_foto']['size'];
-            if (!$this->upload->do_upload('upload_field_name')) {
-                $data['user_foto'] = $this->upload->display_errors();
-            } else {
-                unlink("./dokumen/user/" . $read['user_foto']);
-                unlink("./dokumen/user/thumbs/" . $read['user_foto']);
-                $img = $this->upload->data();
-                $data['user_foto'] = $img['file_name'];
 
-                $file = $this->upload->data();
-                $file_resize_name = $config['upload_path'] . '/' . $file['file_name'];
-                $resize = array();
-                $size   =  array(
-                    array('name' => 'thumbs/', 'width' => auto, 'height' => 80,  'quality' => '100%'),
-                );
+        $filess = $_FILES['user_foto']['name'] ?? null;
+
+        $config['upload_path']   = "./dokumen/user";
+        $config['file_name']     = gen_uuid($this->User->get_table());
+        $config['allowed_types'] = 'jpg|JPG|jpeg|JPEG|png|PNG';
+
+        if ($filess) {
+            $read = $this->User->read($user_id);
+
+            $_FILES['upload_field_name'] = [
+                'name'     => $_FILES['user_foto']['name'],
+                'type'     => $_FILES['user_foto']['type'],
+                'tmp_name' => $_FILES['user_foto']['tmp_name'],
+                'error'    => $_FILES['user_foto']['error'],
+                'size'     => $_FILES['user_foto']['size']
+            ];
+
+            $this->upload->initialize($config);
+            if (!$this->upload->do_upload('upload_field_name')) {
+                $upload_error = $this->upload->display_errors();
+            } else {
+
+                $img = $this->upload->data();
+                $new_file = $img['file_name'];
+                $data['user_foto'] = $new_file;
+                if (!empty($read['user_foto'])) {
+                    @unlink("./dokumen/user/" . $read['user_foto']);
+                    @unlink("./dokumen/user/thumbs/" . $read['user_foto']);
+                }
+
+                $file_path = $config['upload_path'] . '/' . $new_file;
+
+                $size = [
+                    ['name' => 'thumbs/', 'width' => 200, 'height' => 200, 'quality' => '90%'],
+                ];
+
                 foreach ($size as $r) {
-                    $resize = array(
+                    $resize = [
                         "image_library" => 'gd2',
+                        "source_image"  => $file_path,
+                        "new_image"     => $config['upload_path'] . '/' . $r['name'] . $new_file,
                         "width"         => $r['width'],
                         "height"        => $r['height'],
-                        "quality"       => $r['quality'],
-                        "source_image"  => $file_resize_name,
-                        "new_image"     => $config['upload_path'] . '/' . $r['name'] . $file['file_name']
-                    );
+                        "maintain_ratio" => TRUE,
+                        "quality"       => $r['quality']
+                    ];
                     $this->image_lib->clear();
                     $this->image_lib->initialize($resize);
-                    if (!$this->image_lib->resize()) {
-                        $result_foto  = array(
-                            'success' => false,
-                            'message' => $this->image_lib->display_errors()
-                        );
-                    }
+                    $this->image_lib->resize();
                 }
+                $this->dbmp = $this->load->database(multidb_connect($_ENV['PAJAK_DBNAME']), true);
+
+                $this->dbmp->where('pos_user_id', $user_id);
+                $this->dbmp->update('pos_user', [
+                    'pos_user_photo' => $new_file
+                ]);
             }
         }
-        $operation = $this->User->update($data['user_id'], $data);
+
+        $operation = $this->User->update($user_id, $data);
+
         if ($operation['success'] && $operation['record']) {
             $this->session->set_userdata($operation['record']);
         }
+
         $this->response($operation);
     }
 }
