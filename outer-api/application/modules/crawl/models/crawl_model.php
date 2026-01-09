c172f7cc9f41eb3d3bb22c2704bed91c6ef6b2d9
shirohige007 <farispurnomo35@gmail.com>
2026-01-09 14:33:15 +0700

    feat(outer): penyesuaian penarikan data menjadi perbulan setiap hari

--- outer-api/application/modules/crawl/models/crawl_model.php
+++ outer-api/application/modules/crawl/models/crawl_model.php
@@ -39,32 +39,37 @@ class Crawl_model extends Base_Model
         return $this->db->get_where('tb_setting')->result();
     }
 
-    public function store($npwpd, $source, $records)
+    public function store($npwpd, $source, $records, $start_date, $end_date)
     {
         $wp     = $this->optax->get_where('pajak_wajibpajak', ['wajibpajak_npwpd' => $npwpd])->row();
         if (!$wp) return;
+
+        $this->optax->where('wajibpajak_id', $wp->wajibpajak_id);
+        $this->optax->where('penjualan_tanggal >=', $start_date);
+        $this->optax->where('penjualan_tanggal <=', $end_date);
+        $this->optax->delete('pos_penjualan_pooling');
 
         $items  = [];
         foreach ($records as $record) {
             $items = [
                 'penjualan_id'                  => gen_uuid('pos_penjualan_pooling'),
-                'penjualan_tanggal'             => $record->penjualan_tanggal,
-                'penjualan_kode'                => $record->penjualan_kode,
-                'penjualan_total_item'          => $record->penjualan_total_item,
-                'penjualan_total_qty'           => $record->penjualan_total_qty,
-                'penjualan_sub_total'           => $record->penjualan_sub_total,
-                'penjualan_total_nilai_pajak'   => $record->penjualan_total_nilai_pajak,
-                'penjualan_total_grand'         => $record->penjualan_total_grand,
-                'penjualan_nama_customer'       => $record->penjualan_nama_customer,
-                'penjualan_user_nama'           => $record->penjualan_user_nama,
-                'penjualan_jasa'                => $record->penjualan_jasa,
+                'penjualan_tanggal'             => $record['penjualan_tanggal'],
+                'penjualan_kode'                => $record['penjualan_kode'],
+                'penjualan_total_item'          => $record['penjualan_total_item'],
+                'penjualan_total_qty'           => $record['penjualan_total_qty'],
+                'penjualan_sub_total'           => $record['penjualan_sub_total'],
+                'penjualan_total_nilai_pajak'   => $record['penjualan_total_nilai_pajak'],
+                'penjualan_total_grand'         => $record['penjualan_total_grand'],
+                'penjualan_nama_customer'       => $record['penjualan_nama_customer'],
+                'penjualan_user_nama'           => $record['penjualan_user_nama'],
+                'penjualan_jasa'                => $record['penjualan_jasa'],
                 'penjualan_source'              => $source,
                 'wajibpajak_id'                 => $wp->wajibpajak_id
             ];
         }
 
         if (!empty($items)) {
-            $this->optax->insert_batch('pos_penjualan_pooling', $items);
+            $this->optax->insert_batch('pos_penjualan_pooling', [$items]);
         }
     }
 }
